4565c5535934a390d859b403c52c07c12f8470d0
WhoIsKeyserSoze <gaby.foix@gmail.com>
2022-12-01 13:54:56 +0100

    add api with recommender from list of games

---
+++ api/main.py
@@ -0,0 +1,111 @@
+import uvicorn
+from fastapi import FastAPI, Query
+import pandas as pd
+import numpy as np
+from scipy.sparse.linalg import svds #SVD
+from scipy.linalg import sqrtm
+
+from api.utils import *
+
+app = FastAPI()
+
+# @app.get("/model/")
+# def predict(games_list: list[str] = Query(default=None)):
+#
+#     games_dict = {"games_list":games_list}
+#     games_list = games_dict["games_list"]
+#     print(games_list)
+#     recomendations = ["Hello World of Pain"]
+#     for game in games_list:
+#         recomendations.append(game+"4")
+#     return {"recomendations": recomendations}
+
+@app.get("/model_game_names/")
+def predict(games_list: list[str] = Query(default=None)):
+    games_dict = {"games_list": games_list}
+    gamesList = games_dict["games_list"]
+    latent_dimension = 30
+    df_steam = pd.read_csv('Datasets/steam-200k.csv')
+    df_steam.columns = ["UserID", "Name", "Usage", "HoursPlayed", "0"]
+    df_steam = df_steam.drop(columns='0')
+    for games in gamesList:
+        new_row = {'UserID': 128479999, 'Name': games, 'Usage': 'Play', 'HoursPlayed': 999}
+        df_steam = df_steam.append(new_row, ignore_index=True)
+
+    print(df_steam.loc[df_steam['UserID'] == 128479999])
+
+    df_games = pd.read_csv('Datasets/games.csv')
+    df_games.drop(['main_story', 'main_plus_extras', 'completionist', 'coop', 'versus', 'type', 'developers',
+                   'release_na', 'release_eu', 'release_jp', 'platforms', 'publishers'], axis=1, inplace=True)
+    df_games['all_styles'].fillna((df_games['all_styles'].mean()), inplace=True)
+
+    df_merge = pd.merge(df_steam, df_games, left_on="Name", right_on="title", indicator=True, how='inner')
+    # On garde uniquement les jeux qui ont été entièrement joint
+    # df_merge = df_merge[df_merge._merge != 'left_only']
+    # On supprime la colonne _merge pour rendre le dataset plus lisible
+    df_merge = df_merge.drop(columns='_merge')
+    df_merged_games = df_merge[df_merge.Usage != 'purchase']
+
+    condition = [
+        df_merged_games['HoursPlayed'] >= (0.8 * df_merged_games['all_styles']),
+
+        (df_merged_games['HoursPlayed'] >= 0.6 * df_merged_games['all_styles']) &
+        (df_merged_games['HoursPlayed'] < 0.8 * df_merged_games['all_styles']),
+
+        (df_merged_games['HoursPlayed'] >= 0.4 * df_merged_games['all_styles']) &
+        (df_merged_games['HoursPlayed'] < 0.6 * df_merged_games['all_styles']),
+
+        (df_merged_games['HoursPlayed'] >= 0.2 * df_merged_games['all_styles']) &
+        (df_merged_games['HoursPlayed'] < 0.4 * df_merged_games['all_styles']),
+
+        df_merged_games['HoursPlayed'] >= 0
+    ]
+    values = [5, 4, 3, 2, 1]
+    df_merged_games['rating'] = np.select(condition, values)
+
+    userId_map, inverse_userId_map = generate_id_mappings(df_merged_games.UserID.unique())
+    gameId_map, inverse_gameld_map = generate_id_mappings(df_merged_games.id.unique())
+
+    df_merged_games['mUserId'] = df_merged_games['UserID'].map(inverse_userId_map)
+    df_merged_games['mGameId'] = df_merged_games['id'].map(inverse_gameld_map)
+
+    df_exploit_games = df_merged_games[['mGameId', 'Name', 'genres']].copy()
+    df_exploit_games.to_csv('df_exploit_games.csv', index=False)
+
+    df_exploit_CF = df_merged_games[['mUserId', 'mGameId', 'Name', 'rating']].copy()
+    df_exploit_CF.to_csv('df_exploit_CF.csv', index=False)
+
+    df_exploit_games = df_exploit_games.drop_duplicates()
+
+    R_df = df_exploit_CF.pivot_table(index='mUserId', columns='mGameId', values='rating').fillna(0)
+    R = R_df.values
+
+    user_ratings_mean = np.mean(R, axis=1)
+    R_demeaned = R - user_ratings_mean.reshape(-1, 1)
+
+    U, sigma, Vt = svds(R_demeaned, k=latent_dimension)
+
+    sigma = np.diag(sigma)  # transforme en matrice (50, 50)
+    s_root = sqrtm(sigma)  # racine carré
+
+    Usk = np.dot(U, s_root)
+    skV = np.dot(s_root, Vt)
+
+    predicted_rating = np.dot(Usk, skV)
+    predicted_rating = predicted_rating + user_ratings_mean.reshape(-1, 1)
+
+    prediction_df = pd.DataFrame(predicted_rating, columns=R_df.columns)
+
+    already_rated, predictions = recommend_gamesSVD(prediction_df, 128479999, inverse_userId_map, df_exploit_games,
+                                                    df_exploit_CF, 10)
+
+    games_list = []
+    for title in predictions["Name"]:
+        games_list.append(title)
+    return {"predictions": games_list}
+
+    return predictions
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
---
+++ api/utils.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+def generate_id_mappings(ids_list):
+    userId_map = {new_id: old_id for new_id, old_id in enumerate(ids_list)}
+    inverse_userId_map = {old_id: new_id for new_id, old_id in enumerate (ids_list)}
+    return userId_map, inverse_userId_map
+
+
+def recommend_gamesSVD(predictions_df,
+                       userId,
+                       inverse_user_mapping,
+                       games_df,
+                       original_ratings_df,
+                       num_recommendations=10):
+    # Ordonner les prédictions pour l'utilisateur donné
+    inverse_userId = inverse_user_mapping[userId]
+    sorted_user_predictions = predictions_df.iloc[inverse_userId].sort_values(ascending=False)
+
+    # Recupérer les notes déjà données par l'utilisateur avec les titres de jeux
+    user_data = original_ratings_df[original_ratings_df.mUserId == (inverse_userId)]
+    user_full = (user_data.merge(games_df, how='left', left_on='mGameId', right_on='mGameId').
+                 sort_values(['rating'], ascending=False)
+                 )[['mGameId', 'Name_x', 'rating']]
+    # print(user_full)
+    # print('\n\nL\'utilisateur %d a déjà joué et indirectement noté %d jeux.' % (userId, user_full.shape[0]))
+
+    # Recommander les jeux les mieux notés pas encore joué
+    recommendations = (games_df[~games_df['mGameId'].isin(user_full['mGameId'])].
+                       merge(pd.DataFrame(sorted_user_predictions).reset_index(),
+                             how='left',
+                             left_on='mGameId',
+                             right_on='mGameId').
+                       rename(columns={inverse_userId: 'Predictions'}).
+                       sort_values('Predictions', ascending=False).
+                       iloc[:num_recommendations, :-1]
+                       )
+    return user_full, recommendations
